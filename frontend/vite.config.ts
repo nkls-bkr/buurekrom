5d410bc3b53e37da0982c5ad98ce169873a9f7fe
nkls-bkr <niklas.bruenker@gmail.com>
2026-05-01 21:06:32 +0200

    feat: Implement PWA functionality

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,15 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["buurekrom.svg", "icons.svg"],
+      includeAssets: [
+        "buurekrom.svg",
+        "icons.svg",
+        "favicon.ico",
+        //Apple needs all kinds of different splash screens for all different
+        //screen sizes. If not, a blank white splash screen is shown
+        "apple-touch-icon-180x180.png",
+        "apple-splash-*.png",
+      ],
       manifest: {
         name: "Buurekrom",
         short_name: "Buurekrom",
@@ -23,10 +31,25 @@ export default defineConfig({
         scope: "/",
         icons: [
           {
-            src: "/buurekrom.svg",
-            sizes: "any",
-            type: "image/svg+xml",
-            purpose: "any maskable",
+            src: "/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
