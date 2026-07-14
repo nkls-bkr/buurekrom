e5d73378fd536246d745f902b8e9cf6c381298b6
nkls-bkr <niklas.bruenker@gmail.com>
2026-07-14 19:22:53 +0200

    chore(#NONE): Remove cookie banner

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,7 +2,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
-import { CookieBanner } from "@/shared/components/CookieBanner";
 import { queryClient } from "./query-client";
 import { router } from "./router";
 
@@ -12,7 +11,6 @@ export function App() {
       <TooltipProvider>
         <RouterProvider router={router} />
         <Toaster />
-        <CookieBanner />
       </TooltipProvider>
     </QueryClientProvider>
   );
